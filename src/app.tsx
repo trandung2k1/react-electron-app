1621b26a7498d70bf96750a2b4f1b7e52193dbf0
trandung2k1 <trandungitksnb2001@gmail.com>
2024-05-09 23:28:15 +0700

    init commit

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// eslint-disable-next-line import/no-unresolved
+import MainApp from './MainApp';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <MainApp />
+    </React.StrictMode>,
+);
